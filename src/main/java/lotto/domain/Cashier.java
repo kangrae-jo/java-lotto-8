83bf58652fc1262b27a182b83bb819efb3af2356
kangrae <kangrae0501@gmail.com>
2025-11-01 01:07:45 +0900

    refactor(Cashier): 상수 분리 및 메서드 명 변경
    - 로또 가격과 구매 최대 최소 가격을 상수화
    - 유효성 검증의 의도를 명확히 나타낼 수 있는 메서드 명으로 변경

--- src/main/java/lotto/domain/Cashier.java
+++ src/main/java/lotto/domain/Cashier.java
@@ -2,27 +2,30 @@ package lotto.domain;
 
 public class Cashier {
 
+    private final static int LOTTO_PRICE = 1_000;
+    private final static int MIN_PURCHASE_AMOUNT = 1_000;
+    private final static int MAX_PURCHASE_AMOUNT = 10_000;
+
     private final int money;
 
     public Cashier(int money) {
-        validateMoneyRange(money);
-        validateMoney(money);
+        validatePurchaseRange(money);
+        validateAmountUnit(money);
         this.money = money;
     }
 
-    // TODO: 싱수 분리
     public int getLottoAmount() {
-        return money / 1_000;
+        return money / LOTTO_PRICE;
     }
 
-    private void validateMoneyRange(int money) {
-        if (money < 1_000 || 10_000 < money) {
+    private void validatePurchaseRange(int money) {
+        if (money < MIN_PURCHASE_AMOUNT || MAX_PURCHASE_AMOUNT < money) {
             throw new IllegalArgumentException("[ERROR] 1,000원에서 10,000원까지 구매할 수 있습니다.");
         }
     }
 
-    private void validateMoney(int money) {
-        if (money % 1_000 != 0) {
+    private void validateAmountUnit(int money) {
+        if (money % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException("[ERROR] 1,000원 단위로만 구매할 수 있습니다.");
         }
     }
