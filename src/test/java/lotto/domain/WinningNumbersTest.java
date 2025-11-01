003145a350f5d6b0d6b98bfc4a7db3c4e2ec146f
kangrae <kangrae0501@gmail.com>
2025-11-02 02:13:29 +0900

    test(WinningNumbersTest): 당첨 번호 테스트 추가
    - WinningNumbers는 6개의 LottoNumber를 가짐
    - 기본적으로 Lotto와 비슷한 성질을 가짐
    - 보너스 번호와 당첨 번호는 중복이 되지 않아야함

---
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -0,0 +1,28 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class WinningNumbersTest {
+
+    @Test
+    void 당첨_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
+        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 6, 7), 8))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 당첨_번호에_중복된_숫자가_있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 5), 8))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 당첨_번호와_보너스_번호가_중복되면_예외가_발생한다() {
+        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 6))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
