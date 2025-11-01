df05572591ee1e814ae2a896422e84388ef75993
kangrae <kangrae0501@gmail.com>
2025-11-02 02:58:30 +0900

    test(WinningNumbersTest): 당첨 결과 반환 테스트 추가
    - 당첨 결과를 올바르게 반환하는지 테스트 함

--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -1,8 +1,10 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import lotto.domain.dto.WinningStatistics;
 import org.junit.jupiter.api.Test;
 
 class WinningNumbersTest {
@@ -23,6 +25,28 @@ class WinningNumbersTest {
     void 당첨_번호와_보너스_번호가_중복되면_예외가_발생한다() {
         assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 6))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 정상적으로_Rank별_통계를_반환한다() {
+        WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 7);
+        List<Lotto> lottos = List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),   // 1등
+                new Lotto(List.of(1, 2, 3, 4, 5, 7)),   // 2등
+                new Lotto(List.of(1, 2, 3, 4, 5, 8)),   // 3등
+                new Lotto(List.of(1, 2, 3, 4, 9, 10)),  // 4등
+                new Lotto(List.of(1, 2, 3, 11, 12, 13)),// 5등
+                new Lotto(List.of(1, 2, 14, 15, 16, 17))// NONE
+        );
+
+        WinningStatistics statistics = winningNumbers.evaluateLottos(lottos);
+
+        assertThat(statistics.results().get(Rank.FIRST)).isEqualTo(1);
+        assertThat(statistics.results().get(Rank.SECOND)).isEqualTo(1);
+        assertThat(statistics.results().get(Rank.THIRD)).isEqualTo(1);
+        assertThat(statistics.results().get(Rank.FOURTH)).isEqualTo(1);
+        assertThat(statistics.results().get(Rank.FIFTH)).isEqualTo(1);
+        assertThat(statistics.results().get(Rank.NONE)).isEqualTo(1);
     }
 
 }
