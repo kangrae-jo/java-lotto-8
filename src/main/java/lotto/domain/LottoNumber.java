9c38efebcedc37eb9a067e2121fc672b02f65daa
kangrae <kangrae0501@gmail.com>
2025-11-02 02:54:14 +0900

    feat(LottoNumber): 객체 비교 정확성을 위해 equals 및 hashCode 재정의

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.Objects;
+
 public class LottoNumber {
 
     private int number;
@@ -13,6 +15,22 @@ public class LottoNumber {
         if (number < 1 || 45 < number) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof LottoNumber that)) {
+            return false;
+        }
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 
 }
