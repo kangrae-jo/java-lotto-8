b51b7b959a486b3e4f1e98cfc5dcce3f989d4f5b
kangrae <kangrae0501@gmail.com>
2025-11-02 23:04:28 +0900

    fix(LottoController): 입력 검증 실패 시 즉시 재입력하도록 수정
    - 당첨 번호와 보너스 번호가 잘못되면 각각 바로 재입력 받음

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.function.Supplier;
 import lotto.domain.Cashier;
 import lotto.domain.Lotto;
+import lotto.domain.LottoNumber;
+import lotto.domain.LottoNumbers;
 import lotto.domain.Ticket;
 import lotto.domain.WinningNumbers;
 import lotto.domain.dto.LottoNumbersDto;
@@ -21,23 +23,47 @@ public class LottoController {
     }
 
     public void run(Cashier cashier) {
-        // 구임 금액 입력 받기
-        Ticket ticket = retryUntilValid(() -> readAndCreateTicket());
+        // 금액 입력 및 티켓 발급
+        Ticket ticket = readTicketWithRetry();
 
         // 로또 발급 및 출력
         List<Lotto> lottos = cashier.issueLottos(ticket);
         printLottos(lottos);
 
-        // 당첨 번호 입력 받기
-        WinningNumbers winningNumbers = retryUntilValid(() -> readAndCreateWinningNumbers());
+        // 당첨 번호, 보너스 번호 입
+        WinningNumbers winningNumbers = readWinningNumbersWithRetry();
 
         // 결과 출력
         printResult(lottos, winningNumbers, ticket.money());
     }
 
-    private Ticket readAndCreateTicket() {
-        int money = InputParser.parseMoney(inputView.readPurchaseMoney());
-        return new Ticket(money);
+    private Ticket readTicketWithRetry() {
+        return retryUntilValid(() -> {
+            int money = InputParser.parseMoney(inputView.readPurchaseMoney());
+
+            return new Ticket(money);
+        });
+    }
+
+    private WinningNumbers readWinningNumbersWithRetry() {
+        return retryUntilValid(() -> {
+            LottoNumbers numbers = readNumbersWithRetry();
+            LottoNumber bonus = readBonusNumberWithRetry();
+
+            return new WinningNumbers(numbers, bonus);
+        });
+    }
+
+    private LottoNumbers readNumbersWithRetry() {
+        return retryUntilValid(() ->
+                new LottoNumbers(InputParser.parseWinningNumbers(inputView.readWinningNumbers()))
+        );
+    }
+
+    private LottoNumber readBonusNumberWithRetry() {
+        return retryUntilValid(() ->
+                new LottoNumber(InputParser.parseBonusNumber(inputView.readBonusNumber()))
+        );
     }
 
     private void printLottos(List<Lotto> lottos) {
@@ -46,12 +72,6 @@ public class LottoController {
                 .toList();
 
         OutputView.printLottoNumbers(lottoNumbersDtos);
-    }
-
-    private WinningNumbers readAndCreateWinningNumbers() {
-        List<Integer> numbers = InputParser.parseWinningNumbers(inputView.readWinningNumbers());
-        int bonus = InputParser.parseBonusNumber(inputView.readBonusNumber());
-        return new WinningNumbers(numbers, bonus);
     }
 
     private void printResult(List<Lotto> lottos, WinningNumbers winnings, int money) {
